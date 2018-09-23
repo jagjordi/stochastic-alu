92c13bc7d9fb22f117494d69a0ec1cad50333e10
jagjordi <jordialtayo@gmail.com>
2018-09-23 17:39:00 +0200

    Update LFSR.vhd

--- LFSR/LFSR.vhd
+++ LFSR/LFSR.vhd
@@ -20,7 +20,7 @@ architecture behaviour of LFSR is
         signal feedback_vector : std_logic_vector(size-1 downto 0);
     
 begin
-        feedback_vector <= std_logic_vector(to_unsigned(feedback, feedback'size));
+    feedback_vector <= std_logic_vector(to_unsigned(feedback, feedback'size));
     cells <= std_logic_vector(to_unsigned(seed, cells'size));
     
     pr : process(clk, rst)
@@ -29,14 +29,14 @@ begin
             cells <= std_logic_vector(to_unsigned(seed, cells'size));
         elsif rising_edge(clk) then
             conections: for i in 1 to size-2 loop
-                if (feedback_vector(i) = 1) then
+                if (feedback_vector(i) = '1') then
                     cells(i-1) <= cells(i) XOR cells(0);
                 else
                     cells(i-1) <= cells(i);
                 end if;
             end loop;   
             cells(size-1) <= cells(0);
-                end if;
-        end process;
+        end if;
+    end process;
 end behaviour;    
         
